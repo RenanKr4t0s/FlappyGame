d3771c93ee0a42e607c69f2e4c77cef76bf1ddc6
RenanKr4t0s <redrums88@gmail.com>
2024-02-05 20:48:24 -0300

    Criando os tubos e movimento

--- js/flappy.js
+++ js/flappy.js
@@ -1,4 +1,5 @@
 const main = document.querySelector('[tp-flappy]');
+
 function novoElemento(tagName,className){
     const elem = document.createElement(tagName);
     elem.className = className;
@@ -35,5 +36,37 @@ function ParDeBarreiras(altura,abertura,x){
     this.sortearAbertura();
     this.setX(x);
 }
-const b = new ParDeBarreiras(600,300,800);
-main.appendChild(b.elemento);
+
+// const b = new ParDeBarreiras(600,300,1190);
+// main.appendChild(b.elemento);
+
+function BarreirasProntas(altura,largura,abertura,espaco,notificarPonto){
+    this.pares = [
+        new ParDeBarreiras(altura,abertura,largura),
+        new ParDeBarreiras(altura,abertura,largura+espaco),
+        new ParDeBarreiras(altura,abertura,largura+espaco*2),
+        new ParDeBarreiras(altura,abertura,largura+espaco*3),
+    ];
+    const deslocamento = 3;
+
+    this.animar = ()=>{
+        this.pares.forEach(par=>{
+            par.setX(par.getX()-deslocamento)
+            if(par.getX()<par.getLargura()){
+                par.setX(par.getX()+espaco*this.pares.length)
+                par.sortearAbertura()
+            }
+
+            const meio = largura/2;
+            const cruzouMeio = par.getX()+deslocamento>= meio && par.getX()< meio
+            if(cruzouMeio) notificarPonto()
+        })
+    }
+}
+const barreiras = new BarreirasProntas (400,1500,200,400)
+barreiras.pares.forEach(par =>
+    main.appendChild(par.elemento)
+)
+setInterval(()=>{
+    barreiras.animar()
+}, 20)
